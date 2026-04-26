2ef6750a4e67cee6b58eb506e91529c83b96c591
Your Name <you@example.com>
2026-04-26 22:04:27 +0800

    addddddd

--- database.py
+++ database.py
@@ -2,11 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
-# Локальная строка подключения (для твоего компьютера)
+# На Render используем переменную окружения DATABASE_URL
+# Локально - твоя БД
 LOCAL_DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/campus"
 
-# На Render используем переменную окружения DATABASE_URL
-# Если её нет - используем локальную БД
 DATABASE_URL = os.environ.get("DATABASE_URL", LOCAL_DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
